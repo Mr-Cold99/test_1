9bfd498252342e13545e02148158310a55c7b650
Mr-Cold99 <mrcold1984@gmail.com>
2023-06-11 23:09:17 +0300

    типи даних

--- Data_types.java
+++ Data_types.java
@@ -9,14 +9,14 @@ public class Data_types {
         input=JOptionPane.showInputDialog("input b");
         b=Integer.parseInt(input);
         c=a+b;
-        JOptionPane.showMessageDialog(null, "Suma a&b "+c);
+        JOptionPane.showMessageDialog(null, "Suma a&b \n"+c);
         c=a-b;
-        JOptionPane.showMessageDialog(null,"Riznytsa a&b "+c);
+        JOptionPane.showMessageDialog(null,"Riznytsa a&b \n"+c);
         c=a*b;
-        JOptionPane.showMessageDialog(null,"Dobutok a&b "+c);
+        JOptionPane.showMessageDialog(null,"Dobutok a&b \n"+c);
         double d=a,e=b;
         c=d/e;
-        JOptionPane.showMessageDialog(null,"Chastka a&b "+c);
+        JOptionPane.showMessageDialog(null,"Chastka a&b \n"+c);
         System.out.println(c);
     }
 }
