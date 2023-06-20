a0e6de7b99dc3134fcbc7eecb1fb341db6b09b55
Mr-Cold99 <mrcold1984@gmail.com>
2023-06-20 22:00:33 +0300

    Домашка по методах

--- zirochky_pljusyky.java
+++ zirochky_pljusyky.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class zirochky_pljusyky {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        int high;
-        int widh;
-        int target;
+        int high; //висота малюнка
+        int widh;//ширина малюнка
+        int target;//модифікована лінійка
 
         System.out.println("Введіть висоту:");
         high=in.nextInt();
@@ -14,29 +14,29 @@ public class zirochky_pljusyky {
         System.out.println("Введіть номер лінійки яку потрібно модифікувати:");
         target=in.nextInt();
 
-        engine(high,target,widh);
+        engine(high,target,widh); //engine метод який все збирає
 
         }
-        public static void first_last_line(int widh){
+        public static void first_last_line(int widh){// перша і остання лінійка
         for (int i=0;i<widh;i++){
             System.out.print("-");
         }
             System.out.print("\n");
     }
-        public static void main_line(int widh){
+        public static void main_line(int widh){ // основна лінійка
             System.out.print("|");
             for (int i=1;i<widh-1;i++){
                 System.out.print("*");
             }
             System.out.print("|\n");
         }
-        public static void target_line(int widh){
+        public static void target_line(int widh){ //модифікована лінійка
             for (int i=0;i<widh;i++){
                 System.out.print("+");
             }
             System.out.print("\n");
         }
-        public static void engine(int height,int target,int widh){
+        public static void engine(int height,int target,int widh){//збирає все
             first_last_line(widh);
             for(int i=1;i<height-1;i++){
                 if(i==target-1){
