9c87982886cad09d8d8245115d9df56d0fe3f024
Mr-Cold99 <mrcold1984@gmail.com>
2023-07-18 23:18:41 +0300

    Домашка Лекція 14

---
+++ interface_homevork/Homework_14.java
@@ -0,0 +1,11 @@
+package interface_homevork;
+
+public class Homework_14 {
+    public static void main(String[] args) {
+       Homosapiens driv=new Сhauffeur();
+       driv.Drive();
+       Homosapiens pilot=new Flyer();
+       pilot.Fly();
+
+    }
+}
