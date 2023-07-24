d809def52e12670dd5dedecb10290460526f74a2
Mr-Cold99 <mrcold1984@gmail.com>
2023-07-24 22:26:56 +0300

    Домашка Лекція Abstruct

---
+++ abstact_homework/Homework_Main.java
@@ -0,0 +1,13 @@
+package abstact_homework;
+
+public class Homework_Main {
+    public static void main(String[] args) {
+        Univer prof=new Techer();
+        Univer lamer=new Student();
+        prof.name();
+        prof.age();
+        lamer.name();
+        lamer.age();
+
+    }
+}
